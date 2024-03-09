b04992afa17d26b5fa2830283dd8c466684fb984
IRI2070 <muhammad.ayoubi.business@gmail.com>
2024-03-09 23:42:43 +0330

    reformat file content

--- experiments/language_model/prepare_data.py
+++ experiments/language_model/prepare_data.py
@@ -4,31 +4,33 @@ import sys
 import argparse
 from tqdm import tqdm
 
+
 def tokenize_data(input, output=None, vocab_path=None, max_seq_length=512):
-  # p,t=deberta.load_vocab(vocab_path=None, vocab_type='spm', pretrained_id='deberta-v3-base')
-  p,t=deberta.load_vocab(vocab_path=vocab_path, vocab_type='spm', pretrained_id=None)
-  tokenizer=deberta.tokenizers[t](p)
-  if output is None:
-    output=input + '.spm'
-  all_tokens = []
-  with open(input, encoding = 'utf-8') as fs:
-    for l in tqdm(fs, ncols=80, desc='Loading'):
-      if len(l) > 0:
-        tokens = tokenizer.tokenize(l)
-      else:
-        tokens = []
-      all_tokens.extend(tokens)
+    # p,t=deberta.load_vocab(vocab_path=None, vocab_type='spm', pretrained_id='deberta-v3-base')
+    p, t = deberta.load_vocab(vocab_path=vocab_path, vocab_type='spm', pretrained_id=None)
+    tokenizer = deberta.tokenizers[t](p)
+    if output is None:
+        output = input + '.spm'
+    all_tokens = []
+    with open(input, encoding='utf-8') as fs:
+        for l in tqdm(fs, ncols=80, desc='Loading'):
+            if len(l) > 0:
+                tokens = tokenizer.tokenize(l)
+            else:
+                tokens = []
+            all_tokens.extend(tokens)
 
-  print(f'Loaded {len(all_tokens)} tokens from {input}')
-  lines = 0
-  with open(output, 'w', encoding = 'utf-8') as wfs:
-    idx = 0
-    while idx < len(all_tokens):
-      wfs.write(' '.join(all_tokens[idx:idx+max_seq_length-2]) + '\n')
-      idx += (max_seq_length - 2)
-      lines += 1
+    print(f'Loaded {len(all_tokens)} tokens from {input}')
+    lines = 0
+    with open(output, 'w', encoding='utf-8') as wfs:
+        idx = 0
+        while idx < len(all_tokens):
+            wfs.write(' '.join(all_tokens[idx:idx + max_seq_length - 2]) + '\n')
+            idx += (max_seq_length - 2)
+            lines += 1
 
-  print(f'Saved {lines} lines to {output}')
+    print(f'Saved {lines} lines to {output}')
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', required=True, help='The input data path')
